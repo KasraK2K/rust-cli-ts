16be6a9299a92a345a10cf81dc204719fb9bfd14
Kasra Karami <Kasra_K2K@yahoo.com>
2025-12-10 09:53:24 +0330

    tidy up

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,6 @@ fn main() {
     let files = get_template_files(&flag, args.path);
 
     for f in files {
-        // println!("Generating: {}", f.file_name);
-        // println!("{}", f.content);
-        // println!("{}", f.path);
-        
         let full_path = Path::new(&f.path).join(&f.file_name);
 
         if let Some(parent) = full_path.parent() {
